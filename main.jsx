eeae08efd25e6fb77cc44d17320cb473877816c2
jonwascle <jon@wascle.co.uk>
2026-07-30 09:36:16 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  margin: 0;
+  background: #F6F5F2;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
